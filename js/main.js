b54d3a650cdf9600e1bcade01c7e47e39832f7a6
SHAKIL AHMED <88330594+shakilahmedemon@users.noreply.github.com>
2026-01-06 17:35:29 +0800

    Initial Commit

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import { renderNavbar } from "./nav.js";
-import { initCart } from "./store.js";
-import { renderProducts } from "./products.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  renderNavbar();
-  initCart();
-  renderProducts();
-});
