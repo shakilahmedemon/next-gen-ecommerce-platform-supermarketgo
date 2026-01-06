711505731e156eda28dddd515f06b8ca7033308b
SHAKIL AHMED <88330594+shakilahmedemon@users.noreply.github.com>
2026-01-06 17:35:55 +0800

    Initial Commit

--- js/profile.js
+++
@@ -1,5 +0,0 @@
-export function submitReview(productId, rating, comment) {
-  const reviews = JSON.parse(localStorage.getItem("reviews")) || [];
-  reviews.push({ productId, rating, comment });
-  localStorage.setItem("reviews", JSON.stringify(reviews));
-}
